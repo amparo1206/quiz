103ae6cbb448c0479827709c54adf9ab7d45bcc5
amparo1206 <amparo.frontend@gmail.com>
2022-01-11 16:58:40 +0100

    German the best

--- js/highscores.js
+++ js/highscores.js
@@ -1,6 +1,9 @@
-const highScoreList = document.querySelector('#highScoresList')
-const highScores = JSON.parse(localStorage.getItem('#highScores')) || []
+const highScoreList = document.querySelector('#highScoreList')
+console.log (highScoreList)
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+console.log(highScores)
 
-highScoreList.innerHTML = highScores.map(score =>{
+highScoreList.innerHTML = highScores.map(score => {
+    console.log(score)
     return `<li class="high-score">${score.name} - ${score.score}</li>`
 }).join('')
